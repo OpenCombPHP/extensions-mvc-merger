5d675758a7453408b91ab846ed491bf16cc00807
alee <zq@alee2002.com>
2011-12-09 16:06:49 +0800

    modify for control panel menu

--- bean/widget/control-panel-frame-menu.conf.php
+++ bean/widget/control-panel-frame-menu.conf.php
@@ -2,29 +2,34 @@
 return array(
 	array(
 		'title' => '高级网页维护' ,
+		'link' => '?c=org.opencomb.mvcmerger.merger.ControllerBuilder' ,
 		'menu' => array(
 			'direction' => 'v' ,
-			'independence' => true ,
 			'items' => array(
 					array(
 							'title'=>'建立控制器' ,
 							'link' => '?c=org.opencomb.mvcmerger.merger.ControllerBuilder' ,
+							'quote' => 'c=org.opencomb.mvcmerger.merger.ControllerBuilder' ,
 					) ,
 					array(
 							'title'=>'控制器融合' ,
 							'link' => '?c=org.opencomb.mvcmerger.merger.ControllerMerger' ,
+							'quote' => 'c=org.opencomb.mvcmerger.merger.ControllerMerger' ,
 					) ,
 					array(
 							'title'=>'建立模板' ,
 							'link' => '?c=org.opencomb.mvcmerger.merger.TemplateBuilder' ,
+							'quote' => 'c=org.opencomb.mvcmerger.merger.TemplateBuilder' ,
 					) ,
 					array(
 							'title'=>'模板编织' ,
 							'link' => '?c=org.opencomb.mvcmerger.merger.TemplateWeaver' ,
+							'quote' => 'c=org.opencomb.mvcmerger.merger.TemplateWeaver' ,
 					) ,
 					array(
 							'title'=>'视图布局' ,
 							'link' => '?c=org.opencomb.mvcmerger.merger.ViewLayerout' ,
+							'quote' => 'c=org.opencomb.mvcmerger.merger.ViewLayerout' ,
 					) ,
 			) ,
 		) ,
