8c46e8f93e457be7a16d1230fc04949d14c7cd67
lexdene <elephant_liu@mail.dlut.edu.cn>
2012-02-22 11:28:29 +0800

    for rebuild of menu

--- bean/widget/control-panel-frame-menu.conf.php
+++ bean/widget/control-panel-frame-menu.conf.php
@@ -1,37 +1,35 @@
 <?php
 return array(
-	array(
+	'item:mvc-merger' => array(
 		'title' => '高级网页维护' ,
 		'link' => '?c=org.opencomb.mvcmerger.merger.ControllerMerger' ,
-		'menu' => array(
-			'direction' => 'v' ,
-			'items' => array(
-					array(
-							'title'=>'建立网页' ,
-							//'link' => '?c=org.opencomb.mvcmerger.merger.ControllerBuilder' ,
-							//'query' => 'c=org.opencomb.mvcmerger.merger.ControllerBuilder' ,
-					) ,
-					array(
-							'title'=>'网页融合' ,
-							'link' => '?c=org.opencomb.mvcmerger.merger.ControllerMerger' ,
-							'query' => 'c=org.opencomb.mvcmerger.merger.ControllerMerger' ,
-					) ,
-					array(
-							'title'=>'建立模板' ,
-							//'link' => '?c=org.opencomb.mvcmerger.merger.TemplateBuilder' ,
-							//'query' => 'c=org.opencomb.mvcmerger.merger.TemplateBuilder' ,
-					) ,
-					array(
-							'title'=>'模板编织' ,
-							'link' => '?c=org.opencomb.mvcmerger.merger.UITemplateWeave' ,
-							'query' => 'c=org.opencomb.mvcmerger.merger.UITemplateWeave' ,
-					) ,
-					array(
-							'title'=>'视图布局' ,
-							'link' => '?c=org.opencomb.mvcmerger.merger.ViewLayerout' ,
-							'query' => 'c=org.opencomb.mvcmerger.merger.ViewLayerout' ,
-					) ,
-			) ,
+		'direction' => 'v' ,
+		
+		'menu' => 1 ,
+		'item:builder' => array(
+			'title'=>'建立网页' ,
+			//'link' => '?c=org.opencomb.mvcmerger.merger.ControllerBuilder' ,
+			//'query' => 'c=org.opencomb.mvcmerger.merger.ControllerBuilder' ,
+		) ,
+		'item:merger' => array(
+			'title'=>'网页融合' ,
+			'link' => '?c=org.opencomb.mvcmerger.merger.ControllerMerger' ,
+			'query' => 'c=org.opencomb.mvcmerger.merger.ControllerMerger' ,
+		) ,
+		'item:templatebuilder' => array(
+			'title'=>'建立模板' ,
+			//'link' => '?c=org.opencomb.mvcmerger.merger.TemplateBuilder' ,
+			//'query' => 'c=org.opencomb.mvcmerger.merger.TemplateBuilder' ,
+		) ,
+		'item:templateweave' => array(
+			'title'=>'模板编织' ,
+			'link' => '?c=org.opencomb.mvcmerger.merger.UITemplateWeave' ,
+			'query' => 'c=org.opencomb.mvcmerger.merger.UITemplateWeave' ,
+		) ,
+		'item:viewlayerout' => array(
+			'title'=>'视图布局' ,
+			'link' => '?c=org.opencomb.mvcmerger.merger.ViewLayerout' ,
+			'query' => 'c=org.opencomb.mvcmerger.merger.ViewLayerout' ,
 		) ,
 	) ,
 ) ;
